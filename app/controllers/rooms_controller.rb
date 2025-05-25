704fd829ba9ea3d9928a2a1efa71fe05a8ce2a75
Eko Sutrisno Adiguna <73046155+EkoGit-777@users.noreply.github.com>
2025-05-25 14:51:41 +0700

    add include user on message

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -43,14 +43,14 @@ class RoomsController < ApplicationController
   def show
     passcode = request.headers["Passcode"]
     user = User.find_by(passcode: passcode)
-    room = Room.includes(:room_messages, :participant_1, :participant_2)
+    room = Room.includes(room_messages: :user, :participant_1, :participant_2)
       .find_by(code: params[:code])
     if room
       if(room.participant_1_id == nil && room.participant_2_id == nil)
-        render json: room.as_json(include: [:room_messages, :participant_1, :participant_2])
+        render json: room.as_json(include: [room_messages: {include: :user}, :participant_1, :participant_2])
       else
         if(room.participant_1_id == user.id || room.participant_2_id == user.id)
-          render json: room.as_json(include: [:room_messages, :participant_1, :participant_2])
+          render json: room.as_json(include: [:room_messages: {include: :user}, :participant_1, :participant_2])
         else
           render json: { error: "Room not found" }, status: :not_found
         end
@@ -67,22 +67,22 @@ class RoomsController < ApplicationController
     message = params[:message]
     newMessage = RoomMessage.create(user_id: user.id, room_id: params[:room_id], message: message)
 
-    pusher = Pusher::Client.new(
-      app_id: '1997663',
-      key: '965e719119116a6d8f01',
-      secret: '45c9548b64fa36c02742',
-      cluster: 'ap1',
-      encrypted: true
-    )
-
-    pusher.trigger(room.code, 'new-chat', {
-      user: user,
-      date: newMessage.created_at,
-      message: message
-    })
-
     if newMessage
-      render json: newMessage
+      pusher = Pusher::Client.new(
+        app_id: '1997663',
+        key: '965e719119116a6d8f01',
+        secret: '45c9548b64fa36c02742',
+        cluster: 'ap1',
+        encrypted: true
+      )
+
+      pusher.trigger(room.code, 'new-chat', {
+        user: user,
+        date: newMessage.created_at,
+        message: message
+      })
+
+      render json: newMessage.as_json(include: :user)
     else
       render json: { error: "There is something wrong. Please reload the page" }, status: :unprocessable_entity
     end
